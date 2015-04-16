661ef72e3c56d258a690e48130017cf655dc546e
Sergei Filippov <s@haiku.co.nz>
2015-04-16 23:17:17 +1200

    refactor! About time

--- pi.mobile_detect.php
+++ pi.mobile_detect.php
@@ -5,69 +5,54 @@
  *
  * @author      Sergei Filippov
  *
- * @copyright   2014 Haiku Lab Limited
+ * @copyright   2015 Haiku Lab Limited
  * @link        https://github.com/haikulab
  * @license     http://haikulab.co.nz
  * @ref         https://github.com/serbanghita/Mobile-Detect
  */
+
+require_once('Mobile-Detect/Mobile_Detect.php');
+
 class Plugin_Mobile_Detect extends Plugin {
 
    var $meta = array(
         'name'       => 'Mobile Detect',
-        'version'    => '0.1.3',
+        'version'    => '0.2.5',
         'author'     => 'Sergei Filippov',
         'author_url' => 'http://haiku.co.nz'
-    );
+   );
 
-    function __construct() {
-        parent::__construct();
+   function __construct() {
 
-        require_once('Mobile-Detect/Mobile_Detect.php');
+       parent::__construct();
 
-        // todo: fix the detection!!
-        $_folder = $this->addon_location . DIRECTORY_SEPARATOR ."Mobile-Detect";
-        $_file = $_folder . DIRECTORY_SEPARATOR . "Mobile_Detect.php";
-        if (is_dir($_folder)) {
-            if(file_exists($_file)){
-                //include the Mobile Detect class
-                require_once('Mobile-Detect/Mobile_Detect.php');
-            } else {
-                $this->log->fatal("Mobile_Detect.php is missing\n Refer to the README.md, specifically Git Submodules");
-            }
-        }
-    }
-
-    /**
+   }
+   /**
     * Detect what type of device is requesting the page
     *
     * @param   none
     * @return  string $deviceType
-    */
+   */
+   public function index() {
 
-    public function index() {
+       // instantiate Mobile_Detect Object
+       $detect = new Mobile_Detect;
 
-        // instantiate Mobile_Detect Object
-        $detect = new Mobile_Detect;
+       // return string value of device type, else return string "computer"
+       return ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
+   }
 
-        // get the device type e.g. Mobile, Tablet, Computer
-        $deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
+   /**
+    * Lets you know what version of the Mobile Detect library you are using
+    *
+    * @param   none
+    * @return  string $scriptVersion
+   */
+   public function library_version() {
 
-        // return string value of device type, else return string "computer"
-        return $deviceType;
-    }
+       // instantiate Mobile_Detect Object
+       $detect = new Mobile_Detect;
 
-    /**
-     * Lets you know what version of the Mobile Detect library you are using
-     *
-     * @param   none
-     * @return  string $scriptVersion
-     */
-    public function library_version() {
-
-        $detect = new Mobile_Detect; // instantiate Mobile_Detect Object
-
-        $scriptVersion = $detect->getScriptVersion(); // get version of the Mobile Detector library
-
-        return $scriptVersion;
-    }
+       return $detect->getScriptVersion();
+   }
 }
